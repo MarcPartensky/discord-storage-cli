b29bddf55d1754d28ff9ee221e47553eff516b28
Marc Partensky <marc.partensky@gmail.com>
2025-01-02 16:54:47 +0100

    dotenv

--- main.py
+++ main.py
@@ -10,61 +10,13 @@ Functions:
     - Receive file from discord then decrypt
 """
 
-# import os
-# import hashlib
-# import ecies
-# import argparse
-# from ecdsa import SECP256k1, SigningKey
-# from ecdsa.util import string_to_number
-
-# # 1. Générer la clé privée à partir du mot de passe avec PBKDF2
-# # password = b"monMotDePasseSecret!"
-# SALT = os.environ["SALT"].encode()
-# DISCORD_BOT_API_URL = os.environ["DISCORD_BOT_API_URL"]
-# # ITERATIONS = os.environ["ITERATIONS"]
-# ITERATIONS = 100000
-
-# # Générer une clé privée sécurisée avec PBKDF2
-# private_key_bytes = hashlib.pbkdf2_hmac('sha256', input("password").encode(), SALT, ITERATIONS, dklen=32)
-
-# # Vérifier que la clé privée est valide pour secp256k1
-# order = SECP256k1.order
-# private_key_int = string_to_number(private_key_bytes) % order
-
-# # 2. Créer la clé privée et la clé publique
-# sk = SigningKey.from_secret_exponent(private_key_int, curve=SECP256k1)
-# public_key = sk.get_verifying_key().to_string("compressed")
-
-# # 3. Signer un message (par exemple, le texte à chiffrer)
-# message = b"Hello, ECIES avec signature !"
-# message_hash = hashlib.sha256(message).digest()
-# signature = sk.sign(message_hash)
-
-# # 4. Chiffrer le message signé avec la clé publique du destinataire (exemple avec une clé publique générique)
-# recipient_public_key = public_key  # Utiliser la clé publique du destinataire ici
-# encrypted_message = ecies.encrypt(recipient_public_key, message + signature)
-
-# # 5. Déchiffrer le message
-# decrypted_message = ecies.decrypt(private_key_bytes, encrypted_message)
-
-# # Le message décrypté doit contenir le texte original + la signature
-# original_message = decrypted_message[:-len(signature)]  # Extraire le message original
-# received_signature = decrypted_message[-len(signature):]  # Extraire la signature
-
-# # Vérifier la signature avec la clé publique
-# verifying_key = sk.get_verifying_key()
-# assert verifying_key.verify(received_signature, hashlib.sha256(original_message).digest())
-
-# print("Message original :", original_message.decode())
-
 import hashlib
-
-# import os
 import getpass
 import ecies
 import fire
 from ecdsa import SECP256k1, SigningKey
 from ecdsa.util import string_to_number
+from dotenv import load_dotenv
 
 
 class ECCManager:
@@ -162,4 +114,5 @@ class ECCManager:
 
 
 if __name__ == "__main__":
+    load_dotenv()
     fire.Fire(ECCManager)
